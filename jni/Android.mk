6345dbf13edbfab2e17d4afcef6f3acb75ba8a02
Abel Luck <abel@guardianproject.info>
2012-08-09 15:02:26 -0400

    Change project config to support native code debugging.

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,9 +4,11 @@ include $(CLEAR_VARS)
 
 external         := $(NDK_APP_PROJECT_PATH)/external
 prefix           := data/data/info.guardianproject.gpg/app_opt
+APP_OPTIM        := debug
 LOCAL_MODULE     := libgpgme-for-java
 LOCAL_C_INCLUDES := $(external)/$(prefix)/include $(external)/gpgme/src
 LOCAL_LDFLAGS    := -L$(external)/$(prefix)/lib -lgpgme
+LOCAL_LDLIBS     += -L$(external)/$(prefix)/lib -lgpgme
 LOCAL_SRC_FILES  := \
 	GnuPGContext.c \
 	GnuPGData.c \
