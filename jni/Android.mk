196724d680977c184982ae7664d0a2652ac54eb1
Hans-Christoph Steiner <hans@eds.org>
2012-08-16 17:56:04 -0400

    rename NDK lib to match the project name: gnupg-for-java

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 external         := $(NDK_APP_PROJECT_PATH)/external
 prefix           := data/data/info.guardianproject.gpg/app_opt
 APP_OPTIM        := debug
-LOCAL_MODULE     := libgpgme-for-java
+LOCAL_MODULE     := libgnupg-for-java
 LOCAL_C_INCLUDES := $(external)/$(prefix)/include $(external)/gpgme/src
 LOCAL_LDFLAGS    := -L$(external)/$(prefix)/lib -lgpgme
 LOCAL_LDLIBS     += -L$(external)/$(prefix)/lib -lgpgme
