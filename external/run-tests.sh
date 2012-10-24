217f428594c1c44d80b574adb81549a55bb0cb1f
Hans-Christoph Steiner <hans@eds.org>
2012-10-23 21:37:17 -0400

    build basic gpgme tests and include script to run them
    This is only a small set of the tests included in gpgme/tests, but these
    are the ones that currently build and run in some useful way.

---
+++ external/run-tests.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+#
+# this script runs the tests on Android
+
+# stop on any error
+set -e
+
+export HOME=/data/data/info.guardianproject.gpg/app_home
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/data/info.guardianproject.gpg/app_opt/lib
+
+findusername_helper() {
+    echo $2
+}
+
+findusername() {
+    echo `findusername_helper $(ls -ld /data/data/info.guardianproject.gpg/app_home)`
+}
+
+runtest() {
+    echo "Running $1/$2 $3 $4 $5 $6 $7 $8 $9"
+    cd /data/data/info.guardianproject.gpg/app_opt/tests/$1
+    su $USER ./$2 $3 $4 $5 $6 $7 $8 $9 && echo DONE.
+}
+
+
+USER=`findusername`
+
+cd /data/data/info.guardianproject.gpg/app_opt/tests/
+runtest gpgme t-version
+runtest gpgme t-engine-info
+runtest gpgme t-data
+runtest gpgme run-import --verbose pubkey-1.asc
+runtest gpgme run-import --verbose pubdemo.asc
+runtest gpgme run-import --verbose pubkey-1.asc
+runtest gpgme run-import --verbose seckey-1.asc
+runtest gpgme run-import --verbose secdemo.asc
+runtest gpgme run-keylist --verbose
