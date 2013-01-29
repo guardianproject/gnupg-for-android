782d6ab66998593a7d4329a3d50848a1f251a6aa
Abel Luck <abel@guardianproject.info>
2013-01-29 18:23:53 -0500

    remove the java unix domain sockets class
    doing domain sockets in java was a rabbit hole, we're doing them in the
    command handler natively instead.
    
    also add a few more ui elements to the prompt

--- src/info/guardianproject/gpg/pinentry/PINEntry.java
+++ src/info/guardianproject/gpg/pinentry/PINEntry.java
@@ -3,23 +3,39 @@ package info.guardianproject.gpg.pinentry;
 import info.guardianproject.gpg.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class PINEntry extends Activity {
-
-	ServerSocketThread mServerSocket;
 	public static String SOCKET_ADDRESS = "info.guardianproject.gpg.pinentry";
+
+
+	public Handler mHandler;
+
+	private EditText pinEdit;
+	private TextView statusView;
+	private Button okButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if( mServerSocket != null ) {
-        	mServerSocket.setStopThread(true);
-        	mServerSocket.stop();
-        }
-        mServerSocket = new ServerSocketThread();
-        mServerSocket.start();
+        statusView = (TextView) findViewById(R.id.statusLabel);
+        okButton = (Button) findViewById(R.id.okButton);
+        pinEdit = (EditText) findViewById(R.id.pinEdit);
+
+        okButton.setOnClickListener( new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+			}
+		});
 
         setContentView(R.layout.activity_pinentry);
     }
