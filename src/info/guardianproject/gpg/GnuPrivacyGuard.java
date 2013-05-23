8693c7c3958b11a4e3d4a157a8cf8c70973e663e
Hans-Christoph Steiner <hans@eds.org>
2013-05-23 16:32:01 -0400

    setup APG Intent test buttons

--- src/info/guardianproject/gpg/GnuPrivacyGuard.java
+++ src/info/guardianproject/gpg/GnuPrivacyGuard.java
@@ -1,11 +1,14 @@
 package info.guardianproject.gpg;
 
+import info.guardianproject.gpg.apg_compat.Apg;
+
 import java.io.File;
 import java.io.OutputStream;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
@@ -25,10 +28,14 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnCreateContextMenuListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.freiheit.gnupg.GnuPGContext;
 
@@ -60,6 +67,8 @@ public class GnuPrivacyGuard extends Activity implements OnCreateContextMenuList
 		setContentView(R.layout.main);
 		consoleScroll = (ScrollView) findViewById(R.id.consoleScroll);
 		consoleText = (TextView) findViewById(R.id.consoleText);
+
+		wireTestButtons();
 	}
 
 	@Override
@@ -81,6 +90,35 @@ public class GnuPrivacyGuard extends Activity implements OnCreateContextMenuList
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.options_menu, menu);
 		return true;
+	}
+
+	@Override
+	protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+		Log.i(GPGApplication.TAG, "Activity Result: " + requestCode + " " + resultCode);
+		if (resultCode == RESULT_CANCELED || data == null) return;
+		Bundle extras = data.getExtras();
+		if (extras != null) {
+			String text = "RESULT: ";
+			switch (requestCode) {
+			case ApgId.SELECT_SECRET_KEY:
+				long keyId = extras.getLong(Apg.EXTRA_KEY_ID);
+				String userId = extras.getString(Apg.EXTRA_USER_ID);
+				text += userId + " " + Long.toHexString(keyId);
+				break;
+			case ApgId.SELECT_PUBLIC_KEYS:
+				long[] selectedKeyIds = extras.getLongArray(Apg.EXTRA_SELECTION);
+				String[] userIds = extras.getStringArray(Apg.EXTRA_USER_IDS);
+				if (selectedKeyIds != null && userIds != null)
+					for (int i = 0; i < selectedKeyIds.length && i < userIds.length; i++) {
+						text += userIds[i] + " " + Long.toHexString(selectedKeyIds[i]) + " ";
+						Log.i(GPGApplication.TAG, "received: " + userIds[i] + " " + Long.toHexString(selectedKeyIds[i]));
+					}
+				break;
+			default:
+				text += "unknown intent";
+			}
+			Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+		}
 	}
 
 	@Override
@@ -224,6 +262,61 @@ public class GnuPrivacyGuard extends Activity implements OnCreateContextMenuList
 
 		if (commandFinishedReceiver != null)
 			unregisterReceiver(commandFinishedReceiver);
+	}
+
+    public static class ApgId {
+        public static final String VERSION = "1";
+
+        public static final String EXTRA_INTENT_VERSION = "intentVersion";
+
+        public static final int DECRYPT = 0x21070001;
+        public static final int ENCRYPT = 0x21070002;
+        public static final int SELECT_PUBLIC_KEYS = 0x21070003;
+        public static final int SELECT_SECRET_KEY = 0x21070004;
+        public static final int GENERATE_SIGNATURE = 0x21070005;
+    }
+
+    private void setOnClick(Button button, final String intentName, final int intentId) {
+		button.setOnClickListener(new OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new android.content.Intent(intentName);
+                intent.putExtra(ApgId.EXTRA_INTENT_VERSION, ApgId.VERSION);
+                try {
+                    startActivityForResult(intent, intentId);
+                    Toast.makeText(view.getContext(),
+                            "started " + intentName + " " + intentId,
+                            Toast.LENGTH_SHORT).show();
+                    Log.i(GPGApplication.TAG, "started " + intentName + " " + intentId);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(view.getContext(),
+                                   R.string.error_activity_not_found,
+                                   Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+	private void wireTestButtons() {
+		Button selectPublicKeysButton = (Button) findViewById(R.id.select_public_keys);
+		setOnClick(selectPublicKeysButton, Apg.Intent.SELECT_PUBLIC_KEYS, ApgId.SELECT_PUBLIC_KEYS);
+
+		Button selectSecretKeyButton = (Button) findViewById(R.id.select_secret_key);
+		setOnClick(selectSecretKeyButton, Apg.Intent.SELECT_SECRET_KEY, ApgId.SELECT_SECRET_KEY);
+
+		Button encryptButton = (Button) findViewById(R.id.encrypt);
+		setOnClick(encryptButton, Apg.Intent.ENCRYPT, ApgId.ENCRYPT);
+
+		Button encryptFileButton = (Button) findViewById(R.id.encrypt_file);
+		setOnClick(encryptFileButton, Apg.Intent.ENCRYPT_FILE, ApgId.ENCRYPT);
+
+		Button decryptButton = (Button) findViewById(R.id.decrypt);
+		setOnClick(decryptButton, Apg.Intent.DECRYPT, ApgId.DECRYPT);
+
+		Button decryptFileButton = (Button) findViewById(R.id.decrypt_file);
+		setOnClick(decryptFileButton, Apg.Intent.DECRYPT_FILE, ApgId.DECRYPT);
+
+		Button generateSignatureButton = (Button) findViewById(R.id.generate_signature);
+		setOnClick(generateSignatureButton, Apg.Intent.GENERATE_SIGNATURE, ApgId.GENERATE_SIGNATURE);
 	}
 
 	// TODO if GpgAgentService needs to send replies, then implement
