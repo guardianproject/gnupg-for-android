eeeeb9ef55d0f580782fa490f9cd0cb37e6827ba
Abel Luck <abel@guardianproject.info>
2013-06-09 23:26:55 +0200

    resolve pinentry orientation changing issues
    we're treating PinEntryActivity as a dialog that once dismissed
    (either through back button, or some other event) CANNOT be reactivated.
    The fact the pinentry dialog closed is propagated back to gpg-agent.
    
    This mimicks the behavior of pinentry on the desktop, if you kill it,
    click cancel, or click the X in your window manager, it dies and gpg-agent
    must RELAUNCH pinentry if it wants to get the pin again.

--- src/info/guardianproject/gpg/pinentry/PinEntryActivity.java
+++ src/info/guardianproject/gpg/pinentry/PinEntryActivity.java
@@ -62,7 +62,6 @@ public class PinEntryActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pinentry);
         NativeHelper.setup(this);
