764bddca4b06602258e5977be0047914e5477a1c
Abel Luck <abel@guardianproject.info>
2013-06-13 12:29:00 +0200

    Add GnuPG account type
    With this commit the "Gnupg" account stype shows up under the
    Accounts settings page.

---
+++ src/info/guardianproject/gpg/sync/AuthenticationService.java
@@ -0,0 +1,37 @@
+package info.guardianproject.gpg.sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class AuthenticationService extends Service {
+
+    private static final String TAG = AuthenticationService.class.getSimpleName();
+    private Authenticator mAuthenticator;
+
+    @Override
+    public void onCreate() {
+        if (Log.isLoggable(TAG, Log.VERBOSE)) {
+            Log.v(TAG, "Authentication Service started.");
+        }
+        mAuthenticator = new Authenticator(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (Log.isLoggable(TAG, Log.VERBOSE)) {
+            Log.v(TAG, "Authentication Service stopped.");
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        if (Log.isLoggable(TAG, Log.VERBOSE)) {
+            Log.v(TAG, "getBinder()...  returning the AccountAuthenticator binder for intent "
+                    + intent);
+        }
+        return mAuthenticator.getIBinder();
+    }
+
+}
---
+++ src/info/guardianproject/gpg/sync/Authenticator.java
@@ -0,0 +1,71 @@
+package info.guardianproject.gpg.sync;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+
+public class Authenticator extends AbstractAccountAuthenticator {
+
+    private final static String TAG = Authenticator.class.getSimpleName();
+
+    // Authentication Service context
+    private final Context mContext;
+
+    public Authenticator(Context context) {
+        super(context);
+        mContext = context;
+    }
+
+    @Override
+    public Bundle addAccount(AccountAuthenticatorResponse response, String accountType,
+            String authTokenType, String[] requiredFeatures, Bundle options)
+            throws NetworkErrorException {
+        Log.v(TAG, "addAccount()");
+        return null;
+    }
+
+    @Override
+    public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account,
+            Bundle options) throws NetworkErrorException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account,
+            String authTokenType, Bundle options) throws NetworkErrorException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getAuthTokenLabel(String authTokenType) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account,
+            String[] features) throws NetworkErrorException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account,
+            String authTokenType, Bundle options) throws NetworkErrorException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
