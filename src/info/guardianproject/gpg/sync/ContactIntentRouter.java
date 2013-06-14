5ba749d45095bdd5708b72ac60209b7bae8d96ca
Abel Luck <abel@guardianproject.info>
2013-06-14 22:00:18 +0200

    skeleton for handle actions from the contact app #1438
    The ContactIntentRouter catches intents sent from a user clicking
    one of our actions in the People app and routes them to the appropriate
    activity.
    
    There currently are no activities, so it finish()es right away.

---
+++ src/info/guardianproject/gpg/sync/ContactIntentRouter.java
@@ -0,0 +1,44 @@
+
+package info.guardianproject.gpg.sync;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+/**
+ * This class catches intents from our contact integration. It then forwards
+ * these intents to the actual Activities that can should handle the requested
+ * action. This activity has no actual UI.
+ */
+public class ContactIntentRouter extends Activity {
+    private static final String TAG = ContactIntentRouter.class.getSimpleName();
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handleIntent();
+    }
+
+    void handleIntent() {
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+        Uri data = intent.getData();
+
+        if (action.equals(Intent.ACTION_VIEW) && type != null) {
+            if (type.equals(SyncAdapterColumns.MIME_PROFILE) && data != null) {
+                Log.d(TAG, "got ACTION_VIEW for typ=" + type + " and dat=" + data);
+                // TODO do stuff here
+            }
+        }
+        // if we get here that means we didn't handle the activity
+        finish();
+    }
+}
