64eae3fc9aad75404bdb9c501b8ceeb7d40c2ce1
Abel Luck <abel@guardianproject.info>
2013-06-14 12:05:57 +0200

    format AndroidManifest and add some docs to the Auth system

--- src/info/guardianproject/gpg/sync/AuthenticationService.java
+++ src/info/guardianproject/gpg/sync/AuthenticationService.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 
+/**
+ * Service responsible for instantiating our Authenticator
+ */
 public class AuthenticationService extends Service {
 
     private static final String TAG = AuthenticationService.class.getSimpleName();
--- src/info/guardianproject/gpg/sync/Authenticator.java
+++ src/info/guardianproject/gpg/sync/Authenticator.java
@@ -1,3 +1,4 @@
+
 package info.guardianproject.gpg.sync;
 
 import android.accounts.AbstractAccountAuthenticator;
@@ -8,6 +9,16 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 
+/**
+ * We don't do traditional authentication like a web service would.
+ * So this class is very bare bones, but still required.
+ *
+ * Conceivably the "properties" methods could be used to invoke
+ * an Activity whereby the user is able to configure his primary
+ * PGP identity.
+ *
+ * Account creation is handled by calling AccountManager.addAccountExplicitly()
+ */
 public class Authenticator extends AbstractAccountAuthenticator {
 
     private final static String TAG = Authenticator.class.getSimpleName();
@@ -31,40 +42,40 @@ public class Authenticator extends AbstractAccountAuthenticator {
     @Override
     public Bundle confirmCredentials(AccountAuthenticatorResponse response, Account account,
             Bundle options) throws NetworkErrorException {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     @Override
     public Bundle editProperties(AccountAuthenticatorResponse response, String accountType) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     @Override
     public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account,
             String authTokenType, Bundle options) throws NetworkErrorException {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     @Override
     public String getAuthTokenLabel(String authTokenType) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     @Override
     public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account,
             String[] features) throws NetworkErrorException {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     @Override
     public Bundle updateCredentials(AccountAuthenticatorResponse response, Account account,
             String authTokenType, Bundle options) throws NetworkErrorException {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
