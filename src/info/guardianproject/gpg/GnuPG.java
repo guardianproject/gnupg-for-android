5c12e4e508b794ac15e8c2bbb350648955921438
Hans-Christoph Steiner <hans@eds.org>
2013-06-14 21:18:46 -0400

    add common way to call the gpg2 binary from the command line

--- src/info/guardianproject/gpg/GnuPG.java
+++ src/info/guardianproject/gpg/GnuPG.java
@@ -1,5 +1,7 @@
 package info.guardianproject.gpg;
 
+import android.util.Log;
+
 import com.freiheit.gnupg.GnuPGContext;
 
 public class GnuPG {
@@ -10,5 +12,20 @@ public class GnuPG {
 		// set the homeDir option to our custom home location
 		context.setEngineInfo(context.getProtocol(), context.getFilename(),
 				NativeHelper.app_home.getAbsolutePath());
+	}
+
+	public static void gpg2(String args) {
+		final String TAG = "gpg2";
+		String command = NativeHelper.gpg2 + " " + args;
+		Log.i(TAG, command);
+		try {
+			Runtime.getRuntime()
+					.exec(command, NativeHelper.envp, NativeHelper.app_home)
+					.waitFor();
+			Log.i(TAG, "finished: " + command);
+		} catch (Exception e) {
+			Log.e(TAG, "FAILED: " + command, e);
+		}
+
 	}
 }
