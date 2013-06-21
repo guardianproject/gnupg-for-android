2c5eb3b60f32a2f42bafbf01e02b18d4dccbb59c
Hans-Christoph Steiner <hans@eds.org>
2013-06-21 17:44:24 -0400

    gpgme decryption does not currently work, handle Intents with gpg2 cmd line
    This currently only handles incoming file URLs, like from ASTRO File
    Manager.  It does not handle attachments from Gmail since those come in as
    a stream/ContentProvider. That should probably wait until we get gpgme
    decryption working.

--- src/info/guardianproject/gpg/FileHandlerActivity.java
+++ src/info/guardianproject/gpg/FileHandlerActivity.java
@@ -1,36 +1,35 @@
 package info.guardianproject.gpg;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-
-import org.apache.commons.io.FileUtils;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
 import android.widget.Toast;
-
-import com.freiheit.gnupg.GnuPGData;
-import com.freiheit.gnupg.GnuPGException;
 
 public class FileHandlerActivity extends Activity {
 	public static final String TAG = "FileHandlerActivity";
 
 	public static final String[] extensions = { ".asc", ".gpg", ".pgp", ".pkr", ".sig" };
 
+	private MimeTypeMap mMap;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		mMap = MimeTypeMap.getSingleton();
+
 		// Get intent, action and MIME type
 		Intent intent = getIntent();
 		String action = intent.getAction();
-		String type = intent.getType();
+		String mimeType = intent.getType();
 		File incomingFile;
 		try {
 			incomingFile = new File(new URI(intent.getDataString()).getPath());
@@ -38,9 +37,49 @@ public class FileHandlerActivity extends Activity {
 			e1.printStackTrace();
 			return;
 		}
-		Log.i(TAG, "action: " + action + "   MIME Type: " + type + "   data: "
+		Log.i(TAG, "action: " + action + "   MIME Type: " + mimeType + "   data: "
 				+ incomingFile);
 
+		String incomingFileName = incomingFile.getAbsolutePath();
+		String extension = MimeTypeMap.getFileExtensionFromUrl(incomingFileName);
+		String mimeTypeFromExtension = mMap.getMimeTypeFromExtension(extension);
+		String plainFilename = incomingFileName.substring(0, incomingFileName.length() - 4);
+		Log.i(TAG, "plain file: " + plainFilename + "  extention: " + extension
+				+ "  MIME: " + mimeTypeFromExtension);
+
+		if (mimeType.equals("application/octet-stream")) {
+			decryptStream();
+		} else 	if (incomingFile.canRead()) {
+			if (extension.equals("gpg") || extension.equals("pgp")) {
+				decryptFile(incomingFile, plainFilename);
+			} else	if (extension.equals("asc") || extension.equals("pkr")) {
+				//TODO pass off to import after prompting the user
+			} else	if (extension.equals("sig")) {
+				//TODO verify signature
+			}
+		} else {
+			Toast.makeText(this, getString(R.string.error_cannot_read_incoming_file),
+					Toast.LENGTH_LONG).show();
+		}
+		finish();
+	}
+	
+	private class DecryptTask extends AsyncTask<File, Void, Void> {
+
+		@Override
+		protected Void doInBackground(File... params) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
+
+	private void decryptStream() {
+		// TODO implement decrypting a stream
+	}
+
+	private void decryptFile(File incomingFile, String outputFilename) {
+/* this should work, but it doesn't, yet another gpgme issue...
 		GnuPGData cipher;
 		try {
 			byte[] contents = FileUtils.readFileToByteArray(incomingFile);
@@ -62,26 +101,50 @@ public class FileHandlerActivity extends Activity {
 		}
 		cipher.destroy();
 
-		String incomingFileName = incomingFile.getAbsolutePath();
-		String plainFile = incomingFileName.substring(0, incomingFileName.length() - 4);
-		String extension = MimeTypeMap.getFileExtensionFromUrl(plainFile);
-		String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(
-				extension);
-		Log.i(TAG, "plain file: " + plainFile + "  extention: " + extension
-				+ "  MIME: " + mimeType);
-
 		FileOutputStream out;
 		// getCanonicalFile() is a workaround for missing /storage/emulated/0
 		try {
-			out = new FileOutputStream(new File(plainFile).getCanonicalFile());
+			out = new FileOutputStream(new File(plainFilename).getCanonicalFile());
 			plain.write(out);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return;
 		}
+*/
+		File outputFile = new File(outputFilename);
+		if (outputFile.exists()) {
+			String format = getString(R.string.error_output_file_exists_skipping_format);
+			String msg = String.format(format, outputFilename);
+			Log.e(TAG, msg);
+			Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+			finish();
+			return;
+		}
 
-		Intent viewIntent = new Intent(Intent.ACTION_VIEW);
-		viewIntent.setDataAndType(Uri.parse(plainFile), mimeType);
-		startActivity(viewIntent);
+		String args = "--output " + outputFile + " --decrypt " + incomingFile;
+		GnuPG.gpg2(args);
+
+		if (outputFile.exists()) {
+			String extension = MimeTypeMap.getFileExtensionFromUrl(outputFilename);
+			String mimeType = mMap.getMimeTypeFromExtension(extension);
+			Log.i(TAG, "Launching VIEW Intent for " + outputFilename + " of type " + mimeType);
+			Intent viewIntent = new Intent(Intent.ACTION_VIEW);
+			viewIntent.setDataAndType(Uri.parse(outputFilename), mimeType);
+			startActivity(viewIntent);
+		} else {
+			String format = getString(R.string.error_decrypting_file_failed_format);
+			String msg = String.format(format, outputFilename);
+			Log.e(TAG, msg);
+			Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+		}
+		finish();
+	}
+
+	private void verifyFile(File incoming) {
+		
+	}
+
+	private void importFile(File incoming) {
+		
 	}
 }
