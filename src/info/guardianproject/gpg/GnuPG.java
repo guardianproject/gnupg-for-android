12dff7f9d501611e42ee5b4389c75a28240c8230
Hans-Christoph Steiner <hans@eds.org>
2013-07-04 00:49:04 -0400

    in GnuPG.gpg2(), run gpg2 in a shell and output stdout/stderr to logcat
    Using NativeHelper.gpg2 in a CommandThread seems to trigger pinentry fine,
    so I figured perhaps its related to running the command in /system/bin/sh.
    This also dumps stdout/stderr to logcat so we're not flying blind.
    
    refs #1560

--- src/info/guardianproject/gpg/GnuPG.java
+++ src/info/guardianproject/gpg/GnuPG.java
@@ -1,5 +1,10 @@
 package info.guardianproject.gpg;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.util.Log;
 
 import com.freiheit.gnupg.GnuPGContext;
@@ -26,13 +31,35 @@ public class GnuPG {
 		String command = NativeHelper.gpg2 + " " + args;
 		Log.i(TAG, command);
 		try {
-			Runtime.getRuntime()
-					.exec(command, NativeHelper.envp, NativeHelper.app_home)
-					.waitFor();
+            Process sh = Runtime.getRuntime().exec("/system/bin/sh",
+                    NativeHelper.envp, NativeHelper.app_home);
+            OutputStream stdin = sh.getOutputStream();
+            InputStream stdout = sh.getInputStream();
+            InputStream stderr = sh.getErrorStream();
+
+            stdin.write((command + "\nexit\n").getBytes("ASCII"));
+            sh.waitFor();
+
+            Log.i("stdout", readResult(stdout));
+            Log.w("stderr", readResult(stderr));
 			Log.i(TAG, "finished: " + command);
 		} catch (Exception e) {
 			Log.e(TAG, "FAILED: " + command, e);
 		}
-
 	}
+
+    private static String readResult(InputStream i) {
+        String ret = "";
+        try {
+            byte[] readBuffer = new byte[512];
+            int readCount = -1;
+            while ((readCount = i.read(readBuffer)) > 0) {
+                ret += new String(readBuffer, 0, readCount);
+            }
+        } catch (IOException e) {
+            Log.e("GnuPG", "readResult", e);
+        }
+        return ret;
+    }
+
 }
