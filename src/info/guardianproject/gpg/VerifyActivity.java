86acf61130964c41909d04cf143e8896d3f132f3
Hans-Christoph Steiner <hans@eds.org>
2013-07-12 00:59:35 -0400

    verify .sig and .asc detached signatures, and prompt to view file if good
    This verifies the .sig and .asc "--detach-sign" format of files, provided
    the signed file is present as well.  If that's successful, then it'll
    prompt the user to view the file or not.
    
    refs #1569

--- src/info/guardianproject/gpg/VerifyActivity.java
+++ src/info/guardianproject/gpg/VerifyActivity.java
@@ -4,8 +4,10 @@ package info.guardianproject.gpg;
 import java.io.File;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -17,6 +19,10 @@ import android.widget.Toast;
 public class VerifyActivity extends Activity {
     private static final String TAG = "VerifyActivity";
 
+    private String mSignatureFilename;
+    private String mExtension;
+    private String mSignedFilename;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,30 +33,77 @@ public class VerifyActivity extends Activity {
         Uri uri = intent.getData();
         Log.i(TAG, "action: " + action + "   MIME Type: " + mimeType + "   uri: " + uri);
 
-        String signatureFilename = uri.getPath();
-        String extension = MimeTypeMap.getFileExtensionFromUrl(signatureFilename);
-        if (extension.equals("asc")) {
-            new VerifyFileTask(this).execute(signatureFilename);
-        } else if (extension.equals("sig")) {
-            String signedFilename = signatureFilename.replaceAll("\\.sig$", "");
-            if (new File(signedFilename).exists())
-                new VerifyFileTask(this).execute(signatureFilename);
-            else
-                Toast.makeText(this, signedFilename + " does not exist to verify!",
-                        Toast.LENGTH_LONG).show();
+        mSignatureFilename = uri.getPath();
+        mExtension = MimeTypeMap.getFileExtensionFromUrl(mSignatureFilename);
+        /*
+         * though a .asc file could contain data, we currently assume its a
+         * detached sig from "gpg2 --armor --detach-sign". that will need to
+         * change since its possible to include data in an .asc, like from
+         * "gpg2 --clearsign" or maybe even "gpg2 --sign"
+         */
+        if (mExtension.equals("asc") || mExtension.equals("sig")) {
+            mSignedFilename = mSignatureFilename.replaceAll("\\.(asc|sig)$", "");
+            if (new File(mSignedFilename).exists())
+                new VerifyFileTask(this).execute(mSignatureFilename);
+            else {
+                String msg = String.format(getString(R.string.error_file_does_not_exist_format),
+                        mSignatureFilename);
+                showError(msg);
+            }
         } else {
             Log.d(TAG, uri + " not handled yet");
         }
         // TODO verify content:// and streams by using GnuPG.context.verify()
-        finish();
     }
 
     private void verifyComplete(Integer result) {
         Log.d(TAG, "verify complete");
         setResult(result);
-        if (result != RESULT_OK)
-            Toast.makeText(this, "VERIFY FAILED!", Toast.LENGTH_LONG).show();
-        finish();
+        if (result == RESULT_OK) {
+            showSuccess();
+        } else {
+            String msg = String.format(getString(R.string.error_file_verify_failed_format),
+                    mSignatureFilename);
+            showError(msg);
+        }
+    }
+
+    private void showSuccess() {
+        String msg = String.format(getString(R.string.dialog_verify_succeeded_view_file_format),
+                mSignatureFilename);
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.signature_verified)
+                .setMessage(msg)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent view = new Intent(Intent.ACTION_VIEW);
+                        Uri uri = Uri.fromFile(new File(mSignedFilename));
+                        view.setData(uri);
+                        startActivity(Intent.createChooser(view,
+                                getString(R.string.dialog_view_file_using)));
+                        finish();
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        builder.show();
+    }
+
+    private void showError(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.error_verify_failed)
+                .setMessage(msg)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        builder.show();
     }
 
     public class VerifyFileTask extends AsyncTask<String, String, Integer> {
