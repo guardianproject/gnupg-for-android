8b549d404f58d989980786a2b94addc41ee545e5
Hans-Christoph Steiner <hans@eds.org>
2013-07-15 21:02:05 -0400

    add methods to create PGP ASCII pattern matchers to detect ASCII contents
    A .asc file can be a lot of things, so we need to look at the embedded
    headers in order to see what the contents is. This will be very heavy on
    large files, but since we're aiming for ease of use, I think its
    worthwhile to include.  Plus, large files should be binary, not ASCII.
    
    refs #1569 refs #1455 refs #1456

--- src/info/guardianproject/gpg/GnuPG.java
+++ src/info/guardianproject/gpg/GnuPG.java
@@ -1,8 +1,11 @@
+
 package info.guardianproject.gpg;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.util.Log;
 
@@ -10,6 +13,12 @@ import com.freiheit.gnupg.GnuPGContext;
 
 public class GnuPG {
 	public static GnuPGContext context = null;
+
+	private static Pattern PGP_MESSAGE = null;
+	private static Pattern PGP_PRIVATE_KEY_BLOCK = null;
+	private static Pattern PGP_PUBLIC_KEY_BLOCK = null;
+	private static Pattern PGP_SIGNATURE = null;
+	private static Pattern PGP_SIGNED_MESSAGE = null;
 
 	public static void createContext() {
 		context = new GnuPGContext();
@@ -56,4 +65,48 @@ public class GnuPG {
         return ret;
     }
 
+    public static Matcher getPgpMessageMatcher(CharSequence input) {
+        if (PGP_MESSAGE == null)
+            PGP_MESSAGE = Pattern
+                    .compile(
+                            ".*?(-----BEGIN PGP MESSAGE-----.*?-----END PGP MESSAGE-----).*",
+                            Pattern.DOTALL);
+        return PGP_MESSAGE.matcher(input);
+    }
+
+    public static Matcher getPgpSignatureMatcher(CharSequence input) {
+        if (PGP_SIGNATURE == null)
+            PGP_SIGNATURE = Pattern
+                    .compile(
+                            ".*?(-----BEGIN PGP SIGNATURE-----.*?-----END PGP SIGNATURE-----).*",
+                            Pattern.DOTALL);
+        return PGP_SIGNATURE.matcher(input);
+    }
+
+    public static Matcher getPgpSignedMessageMatcher(CharSequence input) {
+        if (PGP_SIGNED_MESSAGE == null)
+            PGP_SIGNED_MESSAGE = Pattern
+                    .compile(
+                            ".*?(-----BEGIN PGP SIGNED MESSAGE-----.*?-----BEGIN PGP SIGNATURE-----.*?-----END PGP SIGNATURE-----).*",
+                            Pattern.DOTALL);
+        return PGP_SIGNED_MESSAGE.matcher(input);
+    }
+
+    public static Matcher getPgpPrivateKeyBlockMatcher(CharSequence input) {
+        if (PGP_PRIVATE_KEY_BLOCK == null)
+            PGP_PRIVATE_KEY_BLOCK = Pattern
+                    .compile(
+                            ".*?(-----BEGIN PGP PRIVATE KEY BLOCK-----.*?-----END PGP PRIVATE KEY BLOCK-----).*",
+                            Pattern.DOTALL);
+        return PGP_PRIVATE_KEY_BLOCK.matcher(input);
+    }
+
+    public static Matcher getPgpPublicKeyBlockMatcher(CharSequence input) {
+        if (PGP_PUBLIC_KEY_BLOCK == null)
+            PGP_PUBLIC_KEY_BLOCK = Pattern
+                    .compile(
+                            ".*?(-----BEGIN PGP PUBLIC KEY BLOCK-----.*?-----END PGP PUBLIC KEY BLOCK-----).*",
+                            Pattern.DOTALL);
+        return PGP_PUBLIC_KEY_BLOCK.matcher(input);
+    }
 }
