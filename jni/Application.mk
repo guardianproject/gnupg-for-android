c3d14d52c88d38493a3763e9cc55be885646e46c
Hans-Christoph Steiner <hans@eds.org>
2013-08-12 20:42:42 -0400

    set NDK_TOOLCHAIN_VERSION and NDK_PLATFORM_LEVEL from the Android files
    make external/Makefile get NDK_TOOLCHAIN_VERSION and NDK_PLATFORM_LEVEL
    from the official Android files for setting those build vars.

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,3 +1,4 @@
-APP_PLATFORM := android-8
+NDK_TOOLCHAIN_VERSION=4.7
 APP_ABI := armeabi
-#APP_ABI := x86
+# the NDK platform level, aka APP_PLATFORM, is equivalent to minSdkVersion
+APP_PLATFORM := android-$(shell sed -n 's,.*android:minSdkVersion="\([0-9][0-9]*\)".*,\1,p' AndroidManifest.xml)
