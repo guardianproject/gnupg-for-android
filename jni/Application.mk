805eff5374475d1561437bdee67f9ba0b02819bd
Hans-Christoph Steiner <hans@eds.org>
2013-12-04 00:38:39 -0500

    build native code for armeabi-v7a, older devices can use Java OpenPGP
    building for armeabi-v7a could give a dramatic speed boost to armv7a CPUs,
    which have been the standard for Android devices for a while.  Since there
    are Java OpenPGP implementations available, we don't need to worry so much
    about supporting the very old or very cheap hardware that does not include
    armv7a CPUs.  This has become more important given all the ARM assembly
    added to libgcrypt recently.
    
    refs #1630

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
 NDK_TOOLCHAIN_VERSION=4.7
-APP_ABI := armeabi
+APP_ABI := armeabi-v7a
 # the NDK platform level, aka APP_PLATFORM, is equivalent to minSdkVersion
 APP_PLATFORM := android-$(shell sed -n 's,.*android:minSdkVersion="\([0-9][0-9]*\)".*,\1,p' AndroidManifest.xml)
