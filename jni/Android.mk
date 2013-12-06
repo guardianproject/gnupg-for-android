f027190860432925903d1574334011766d09fcd4
Hans-Christoph Steiner <hans@eds.org>
2013-12-05 23:02:19 -0500

    install libs built Android-style into libs/$(APP_ABI)/ instead of assets/
    This means that Android handles the rest for us, including bundling and
    unpacking them from the APK, setting the LD_LIBRARY_PATH, etc.
    
    refs #1548

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,14 +1,47 @@
 LOCAL_PATH:= $(call my-dir)
 
-include $(CLEAR_VARS)
+external := $(NDK_APP_PROJECT_PATH)/external
+prefix   := data/data/info.guardianproject.gpg/app_opt
+LOCAL    := $(external)/$(prefix)
 
-external         := $(NDK_APP_PROJECT_PATH)/external
-prefix           := data/data/info.guardianproject.gpg/app_opt
-APP_OPTIM        := debug
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libassuan
+LOCAL_SRC_FILES := $(LOCAL)/lib/libassuan.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgcrypt
+LOCAL_SRC_FILES := $(LOCAL)/lib/libgcrypt.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgpg-error
+LOCAL_SRC_FILES := $(LOCAL)/lib/libgpg-error.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgpgme
+LOCAL_SRC_FILES := $(LOCAL)/lib/libgpgme.so
+LOCAL_EXPORT_C_INCLUDES := $(external)/$(prefix)/include $(external)/gpgme/src
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libksba
+LOCAL_SRC_FILES := $(LOCAL)/lib/libksba.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libnpth
+LOCAL_SRC_FILES := $(LOCAL)/lib/libnpth.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+
+include $(CLEAR_VARS)
 LOCAL_MODULE     := libgnupg-for-java
-LOCAL_C_INCLUDES := $(external)/$(prefix)/include $(external)/gpgme/src
 LOCAL_CFLAGS     += -Wformat -Werror=format-security
-LOCAL_LDLIBS     += -L$(external)/$(prefix)/lib -lgpgme -llog
+LOCAL_LDLIBS     += -llog
+LOCAL_SHARED_LIBRARIES := libgpgme
 LOCAL_SRC_FILES  := \
 	GnuPGContext.c \
 	GnuPGData.c \
@@ -16,15 +49,10 @@ LOCAL_SRC_FILES  := \
 	GnuPGKey.c \
 	GnuPGSignature.c \
 	gpgmeutils.c 
-
 include $(BUILD_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
-
-external         := $(NDK_APP_PROJECT_PATH)/external
-prefix           := data/data/info.guardianproject.gpg/app_opt
-APP_OPTIM        := debug
 LOCAL_MODULE     := libpinentry
 LOCAL_CFLAGS := -DHAVE_CONFIG_H -Wformat -Werror=format-security
 LOCAL_LDLIBS     += -llog
@@ -50,14 +78,12 @@ GENERATE_PINENTRY_ASSUAN_ERRORS_C := \
 	$(shell $(external)/pinentry/assuan/mkerrors \
 		< $(external)/pinentry/assuan/assuan.h \
 		> $(external)/pinentry/assuan/assuan-errors.c)
-
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
 LOCAL_MODULE := libposix
 LOCAL_CFLAGS += -Wformat -Werror=format-security
 LOCAL_LDLIBS += -llog
 LOCAL_SRC_FILES := info_guardianproject_gpg_Posix.c
-
 include $(BUILD_SHARED_LIBRARY)
