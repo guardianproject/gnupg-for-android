1ed7b155e64a63870accc3bddf6817fff3d78f1c
Hans-Christoph Steiner <hans@eds.org>
2014-03-12 17:50:21 -0400

    jenkins-build.sh: set the version code and name based on current date
    This will differentiate the debug builds from the normal builds. The debug
    builds will never be upgraded by the release builds since the debug builds
    will always have a much larger versionCode.  Also, the date it was built
    will show up in the version string, making it easier to identify debug
    builds versus release builds.

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -5,6 +5,16 @@
 set -e
 set -x
 
+# reset version code/name to current date
+versionCodeDate=`date +%s`
+versionNameDate=`date +%Y-%m-%d_%H.%M.%S`
+
+sed -i \
+    -e "s,android:versionCode=\"[0-9][0-9]*\",android:versionCode=\"$versionCodeDate\"," \
+    -e "s,android:versionName=\"\([^\"][^\"]*\)\",android:versionName=\"\1.$versionNameDate\"," \
+    AndroidManifest.xml
+
+
 . ~/.android/bashrc
 make -C external/ distclean clean-assets gitclean
 make -C external/
